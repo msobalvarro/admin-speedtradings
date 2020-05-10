13bb76b85d34bba95805c18bfc0e48812e716a44
msobalvarro <mssrsobalvarro1997@gmail.com>
2020-05-09 22:56:21 -0600

    bug fixed label exchange request

--- src/utils/constanst.js
+++ src/utils/constanst.js
@@ -17,8 +17,8 @@ const portSocket = ":2000"
 
 export const keySecret = "testDevelop"
 
-// export const urlServer = "http://ardent-medley-272823.appspot.com"
-export const urlServer = "http://127.0.0.1" + devPort
+export const urlServer = "http://ardent-medley-272823.appspot.com"
+// export const urlServer = "http://127.0.0.1" + devPort
 
 // export const urlServerSocket = urlServer.replace(devPort, "") + portSocket
 export const urlServerSocket = urlServer
