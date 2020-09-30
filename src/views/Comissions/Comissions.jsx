4be94e43bf7eb0a6d21782213e8fe50b014797dd
Harold Espinoza <haroldesptru@gmail.com>
2020-09-30 11:35:34 -0600

    Finalizada vista de comisiones

--- src/views/Comissions/Comissions.jsx
+++ src/views/Comissions/Comissions.jsx
@@ -4,11 +4,15 @@ import "./Comissions.scss"
 
 // Import components
 import NavigationBar from "../../components/NavigationBar/NavigationBar"
+import Modal from "../../components/Modal/Modal"
 import ActivityIndicator from "../../components/ActivityIndicator/Activityindicator"
 import Swal from "sweetalert2"
 
 // Import utils
-import { Petition, randomKey } from "../../utils/constanst"
+import { Petition, randomKey, copyData } from "../../utils/constanst"
+
+// Import assets
+import AlypayLogo from "../../static/images/alypay.png"
 
 
 const Comissions = () => {
@@ -20,11 +24,17 @@ const Comissions = () => {
         }
     }
 
+    // Estado para almacenar el valor del texto de filtro
+    const [filter, setFilter] = useState('')
+
     // Estado para almacenar la lista  de comisiones
     const [data, setData] = useState([])
 
     // Estado para almacenar los datos de un detalle de comision
     const [dataDetail, setDataDetail] = useState({})
+
+    // Estado que almacena el hash de transacción a la hora de aceptar un pago de comisión
+    const [transactionHash, setTransactionHash] = useState('')
 
     // Estado para indicar sponsor se muestra en el detalle y así cambiar el color de su fila dentro de la tabla
     const [activeDetail, setActiveDetail] = useState(-1)
@@ -32,6 +42,7 @@ const Comissions = () => {
     // Estado para controlar la visibilidad del indicador de carga
     const [loaderList, setLoaderList] = useState(false)
     const [loaderDetail, setLoaderDetail] = useState(false)
+    const [loaderPayment, setLoaderPayment] = useState(false)
 
     // Función para obtener los datos de la lista de comisiones
     const getComissionsData = async _ => {
@@ -68,11 +79,144 @@ const Comissions = () => {
 
             setDataDetail(data)
             setActiveDetail(id)
+            setTransactionHash('')
         } catch (error) {
             Swal.fire('Ha ocurrido un error', error, 'error')
         } finally {
             setLoaderDetail(false)
         }
+    }
+
+    /**
+     * Función para remover un registro dentro de la lista de comisiones
+     * @param {Array} list - Lista de los registros de comisión 
+     * @param {Number} removeItem - Id del registro a remover 
+     * @returns {Array}
+     */
+    const removeItemList = (list, removeItem) => {
+        list = Array.from(list)
+
+        const indexItem = list.map(item => item.id).indexOf(removeItem)
+            
+        list.splice(indexItem, 1)
+
+        return list
+    }
+
+    // Función para aceptar un pago de comisión
+    const onHandleAcceptPayment = async () => {
+        try {
+            setLoaderPayment(true)
+
+            if (!dataDetail.alypay && transactionHash.length === 0) {
+                throw String("El hash de transacción es requerido")
+            }
+
+            // Sí todavía no está activa la comisión se pregúnta si en realidad se quiere proceder con el pago
+            if (!dataDetail.active) {
+                const result = await Swal.fire({
+                    title: "Todavía no se han cumplido las 48 horas necesarias, ¿desea realizar el pago de la comisión?",
+                    text: "La acción no se puede deshacer",
+                    icon: "warning",
+                    showCancelButton: true,
+                    cancelButtonColor: '#ffcb08',
+                    confirmButtonColor: ' #C0392B',
+                    confirmButtonText: 'Sí, realizar pago',
+                    cancelButtonText: 'Cancelar',
+                })
+
+                if (result.dismiss) {
+                    return
+                }
+            }
+
+            // Información a enviar para procesar la solicitud
+            const dataSend = {
+                id: activeDetail,
+                hash: transactionHash.length > 0 ? transactionHash : null
+            }
+
+            const { data } = await Petition.post('/admin/commission/accept', dataSend, header)
+
+            if(data.error) {
+                throw String(data.message)
+            }
+
+            Swal.fire('Listo', 'Pago aprovado con éxito', 'success')
+
+            setData(removeItemList(data, activeDetail))
+            setDataDetail({})
+        } catch (error) {
+            Swal.fire('Ha ocurrido un error', error, 'error')
+        } finally {
+            setLoaderPayment(false)
+        }
+    }
+
+    // Función para rechazar un pago de comisión
+    const onHandleDeclinePayment = async _ => {
+        try {
+            setLoaderPayment(true)
+
+            const result = await Swal.fire({
+                title: "¿Rechazar pago de comisión?",
+                text: "La acción no se puede deshacer",
+                icon: "warning",
+                showCancelButton: true,
+                cancelButtonColor: '#ffcb08',
+                confirmButtonColor: ' #C0392B',
+                confirmButtonText: 'Sí, rechazar pago',
+                cancelButtonText: 'Cancelar',
+            })
+
+            if (result.dismiss) {
+                return
+            }
+
+            // Información a enviar para rechazar la solicitud
+            const dataSend = {
+                id: activeDetail
+            }
+
+            const { data } = await Petition.post('/admin/commission/decline', dataSend, header)
+
+            if(data.error) {
+                throw String(data.message)
+            }
+
+            Swal.fire('Listo', 'Pago rechazado con éxito', 'success')
+
+            setData(removeItemList(data, activeDetail))
+            setDataDetail({})
+        } catch (error) {
+            Swal.fire('Ha ocurrido un error', error, 'error')
+        } finally {
+            setLoaderPayment(false)
+        }
+    }
+
+    /**
+     * Función para filtrar los registro de la lista de las comisiones según la entada en
+     * el campon  de filtro
+     * @param {Array} dataSet - Lista a filtrar 
+     * @returns {Array} - Resultado del filtro
+     */
+    const filterData = (dataSet) => {
+        if(filter.length === 0) {
+            return dataSet
+        }
+
+        const filtered = dataSet.filter(item => {
+            const { sponsor, coin, amount } = item
+
+            return (
+                sponsor.toLowerCase().search(filter) > -1 ||
+                coin.toLowerCase().search(filter) > -1 ||
+                amount.toString().toLowerCase().search(filter) > -1
+            )
+        })
+
+        return filtered
     }
 
     useEffect(_ => {
@@ -89,7 +233,12 @@ const Comissions = () => {
                         <h2>Comisiones</h2>
 
                         <div>
-                            <input type="text" placeholder="Escribe para buscar..." className="field-input"/>
+                            <input
+                                value={filter}
+                                onChange={e => setFilter(e.target.value)}
+                                type="text"
+                                placeholder="Escribe para buscar..."
+                                className="field-input"/>
                             <p>
                                 <span className="pending">Pendiente</span>
                                 <span>|</span>
@@ -119,7 +268,7 @@ const Comissions = () => {
                             </div>
                             <div className="body">
                                 {
-                                    data.map(item => (
+                                    filterData(data).map(item => (
                                         <div
                                             key={randomKey()}
                                             onClick={_ => getComissionDetailData(item.id)}
@@ -149,7 +298,7 @@ const Comissions = () => {
                     }
 
                     {
-                        Object.keys(dataDetail).length > 0 &&
+                        Object.keys(dataDetail).length > 0 && !loaderDetail &&
                         <>
                             <div className="detail-item">
                                 <span className="label">Nombre</span>
@@ -184,8 +333,8 @@ const Comissions = () => {
                                         <span className="label">Estado</span>
                                         <span className={`value ${dataDetail.active ? 'ready' : 'pending'}`}>
                                             {
-                                                dataDetail.status === 1
-                                                ? 'Pagado'
+                                                dataDetail.active
+                                                ? 'Listo para pagar'
                                                 : 'Pendiente'
                                             }
                                         </span>
@@ -195,22 +344,59 @@ const Comissions = () => {
 
                             <div className="detail-item">
                                 <span className="label">Wallet en {dataDetail.symbol}</span>
-                                <span className="value hash">{dataDetail.wallet}</span>
+                                <span
+                                    title="Toca para copiar"
+                                    onClick={_ => copyData(dataDetail.wallet)}
+                                    className="value hash">
+                                    {dataDetail.wallet}
+                                </span>
                             </div>
 
-                            <div className="detail-item">
-                                <span className="label">Hash de transacción</span>
-                                <input type="text" placeholder="Hash de transacción" className="value field-input"/>
-                            </div>
+                            {
+                                !dataDetail.alypay &&
+                                <div className="detail-item">
+                                    <span className="label">Hash de transacción</span>
+                                    <input
+                                        value={transactionHash}
+                                        onChange={e => setTransactionHash(e.target.value)}
+                                        type="text"
+                                        placeholder="Hash de transacción"
+                                        className="value field-input"/>
+                                </div>
+                            }
+
+                            {
+                                dataDetail.alypay &&
+                                <div className="detail-item">
+                                    <div className="alypay-payment">
+                                        <span>&#10003;</span> Pagar con <img src={AlypayLogo} alt="AlyPay"/>
+                                    </div>
+                                </div>
+                            }
 
                             <div className="detail-buttons">
-                                <button className="decline">Rechazar &#10005;</button>
-                                <button className="confirm">Aprobar &#10003;</button>
+                                <button
+                                    onClick={onHandleDeclinePayment}
+                                    className="decline">
+                                    Rechazar &#10005;
+                                </button>
+                                <button
+                                    onClick={onHandleAcceptPayment}
+                                    className="confirm">
+                                    Aprobar &#10003;
+                                </button>
                             </div>
                         </>
                     }
                 </div>
             </div>
+
+            {
+                loaderPayment &&
+                <Modal persist={true} onlyChildren>
+                    <ActivityIndicator size={64} />
+                </Modal>
+            }
         </div>
     )
 }
