90b1120c107b70ec30fed939ce1a5226e4d63e3e
Harold Espinoza <haroldesptru@gmail.com>
2020-10-05 15:04:46 -0600

    Añadida opcion para añadir enlaces e imagenes en el editor de la vista de correos

--- src/views/Mail/Mail.jsx
+++ src/views/Mail/Mail.jsx
@@ -64,6 +64,7 @@ var toolbarOptions = [
   
     [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
     [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+    ['link', 'image'],
   
     [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
     [{ 'font': [] }],
