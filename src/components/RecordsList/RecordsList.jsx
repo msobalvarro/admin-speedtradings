31e11cb66e2714135f6163d3dc033d88886d5c0e
Cesar Garay <cgaray.aguilar97@gmail.com>
2020-12-03 11:02:31 -0600

    Icono de tipo de usuario agregado  a la lista de usuarios

--- src/components/RecordsList/RecordsList.jsx
+++ src/components/RecordsList/RecordsList.jsx
@@ -1,69 +1,92 @@
-import React, { useState } from "react"
+import React, { useState } from 'react'
+// Import icons
+import { ReactComponent as PersonIcon } from '../../static/images/user.svg'
+import { ReactComponent as EnterpriseIcon } from '../../static/images/enterprise.svg'
 
 // Import constant
 import { Moment } from '../../utils/constanst'
 
-const RecordsList = ({ data = [], activeDetail = -1, onDetail = _ => { } }) => {
-    const [filter, setFilter] = useState('')
+const RecordsList = ({ data = [], activeDetail = -1, onDetail = _ => {} }) => {
+  const [filter, setFilter] = useState('')
+  const PERSON_TYPE = 1
 
-    // Componente que representa un articulo de la lista
-    // Registos
-    const itemRecord = (item, index) => {
-        if (
-            item.name.length > 0 && item.name.toLowerCase().search(filter) > -1 ||
-            item.country.length > 0 && item.country.toLowerCase().search(filter) > -1
-        ) {
-            return (
-                <div
-                    className={`row ${activeDetail === item.id_user ? 'active' : ''}`}
-                    key={index}
-                    onClick={_ => onDetail(item.id_user)}>
-                    <span className="name">{item.name}</span>
-                    <span>{item.country}</span>
-                    <span>
-                        {
-                            item.sponsor_email !== null
-                                ? item.sponsor_email
-                                : <i>Sin sponsor</i>
-                        }
-                    </span>
-                    <span><Moment date={item.start_date} /></span>
-                </div>
-            )
-        }
-    }
+  //validar el tipo de usuario
+  const showTypeIcon = type => {
+    if (!type) return ''
 
-    // Componente contenedor de la lista
-    return (
-        <>
-            <div className="sub-header">
-                <h2 className="title">Lista de usuarios</h2>
-
-                <input
-                    value={filter}
-                    onChange={e => setFilter(e.target.value)}
-                    placeholder="Escribe para buscar.."
-                    type="text"
-                    className="text-input" />
-            </div>
-
-            <div className="table records">
-                <div className="header">
-                    <span>Nombre</span>
-                    <span>Pais</span>
-                    <span>Sponsor</span>
-                    <span>Fecha</span>
-                </div>
-
-                {
-                    data
-                        .sort((a, b) =>
-                            (new Date(b.start_date) - new Date(a.start_date)))
-                        .map(itemRecord)
-                }
-            </div>
-        </>
+    return type === PERSON_TYPE ? (
+      <PersonIcon className="icon" fill="#ffcb08" />
+    ) : (
+      <EnterpriseIcon className="icon" fill="#ffcb08" />
     )
+  }
+
+  // Componente que representa un articulo de la lista
+  // Registos
+  const itemRecord = (item, index) => {
+    if (
+      (item.name.length > 0 && item.name.toLowerCase().search(filter) > -1) ||
+      (item.country.length > 0 &&
+        item.country.toLowerCase().search(filter) > -1)
+    ) {
+      return (
+        <div
+          className={`row ${activeDetail === item.id_user ? 'active' : ''}`}
+          key={index}
+          onClick={_ => onDetail(item.id_user)}
+        >
+          <span className="icon-container">
+            {item.type_users && showTypeIcon(item.type_users)}
+          </span>
+          <span className="name">{item.name}</span>
+          <span>{item.country}</span>
+          <span>
+            <Moment date={item.start_date} format="YYYY-MM" />
+          </span>
+        </div>
+      )
+    }
+  }
+
+  // Componente contenedor de la lista
+  return (
+    <>
+      <div className="sub-header">
+        <h2 className="title">Lista de usuarios</h2>
+
+        <input
+          value={filter}
+          onChange={e => setFilter(e.target.value)}
+          placeholder="Escribe para buscar.."
+          type="text"
+          className="text-input"
+        />
+      </div>
+      <div className="leyenda-container">
+        <span>
+          <PersonIcon className="icon" fill="#ffcb08" /> Personal
+        </span>
+        <div className="separator"></div>
+        <span>
+          <EnterpriseIcon className="icon" fill="#ffcb08" />
+          Empresarial
+        </span>
+      </div>
+
+      <div className="table records">
+        <div className="header">
+          <span>Tipo</span>
+          <span>Nombre</span>
+          <span>Pais</span>
+          <span>Fecha</span>
+        </div>
+
+        {data
+          .sort((a, b) => new Date(b.start_date) - new Date(a.start_date))
+          .map(itemRecord)}
+      </div>
+    </>
+  )
 }
 
 export default RecordsList
--- src/views/Users/Users.jsx
+++ src/views/Users/Users.jsx
@@ -13,95 +13,95 @@ import DetailRecords from '../../components/DetailRecords/DetailRecords'
 // Import constants
 import { Petition } from '../../utils/constanst'
 
-
 const Users = () => {
-    const { token } = useSelector(storage => storage.globalStorage)
-    const credentials = {
-        headers: {
-            'x-auth-token': token
-        }
+  const { token } = useSelector(storage => storage.globalStorage)
+  const credentials = {
+    headers: {
+      'x-auth-token': token,
+    },
+  }
+
+  // Estado que almacena la lista de los usuarios
+  const [allUsers, setAllUsers] = useState([])
+  // Estado que almacena el id del usuario a mostrar en detalle
+  const [activeDetail, setActiveDetail] = useState(-1)
+  // Estado que almacena la fecha con la que generarán los reportes
+  const [dateReport, setDateReport] = useState(
+    window.sessionStorage.getItem('date_report') ||
+      moment(new Date()).format('YYYY-MM')
+  )
+  // Estado que almacena el status del indicador de carga
+  const [loader, setLoader] = useState(false)
+
+  // Se obtiene la lista de los usuarios
+  const fetchData = async _ => {
+    try {
+      setLoader(true)
+
+      const { data } = await Petition.get('/admin/records/', credentials)
+
+      if (data.error) {
+        throw String(data.message)
+      }
+
+      setAllUsers(data)
+      console.log(data)
+    } catch (error) {
+      console.error(error)
+      Swal.fire('Ha ocurrido un error', error.toString(), 'error')
+    } finally {
+      setLoader(false)
     }
+  }
 
-    // Estado que almacena la lista de los usuarios
-    const [allUsers, setAllUsers] = useState([])
-    // Estado que almacena el id del usuario a mostrar en detalle
-    const [activeDetail, setActiveDetail] = useState(-1)
-    // Estado que almacena la fecha con la que generarán los reportes
-    const [dateReport, setDateReport] = useState(window.sessionStorage.getItem('date_report') || moment(new Date()).format('YYYY-MM'))
-    // Estado que almacena el status del indicador de carga
-    const [loader, setLoader] = useState(false)
+  useEffect(_ => {
+    fetchData()
+  }, [])
 
-    // Se obtiene la lista de los usuarios
-    const fetchData = async _ => {
-        try {
-            setLoader(true)
+  return (
+    <div className="Users">
+      <header className="header">
+        <div className="row">
+          <span className="label">Fecha Reporte</span>
+          <input
+            type="month"
+            className="text-input"
+            value={dateReport}
+            onChange={e => {
+              const { value } = e.target
 
-            const { data } = await Petition.get('/admin/records/', credentials)
-
-            if (data.error) {
-                throw String(data.message)
-            }
-
-            setAllUsers(data)
-        } catch (error) {
-            console.error(error)
-            Swal.fire('Ha ocurrido un error', error.toString(), 'error')
-        } finally {
-            setLoader(false)
-        }
-    }
-
-    useEffect(_ => {
-        fetchData()
-    }, [])
-
-    return (
-        <div className="Users">
-            <header className="header">
-                <div className="row">
-                    <span className="label">Fecha Reporte</span>
-                    <input
-                        type="month"
-                        className="text-input"
-                        value={dateReport}
-                        onChange={e => {
-                            const { value } = e.target
-
-                            if (value) {
-                                setDateReport(value)
-                                window.sessionStorage.setItem('date_report', value)
-                            }
-                        }} />
-                </div>
-            </header>
-
-            <div className="content">
-                <div className="content-list">
-                    {
-                        loader &&
-                        <ActivityIndicator size={64} />
-                    }
-
-                    {
-                        !loader && allUsers.length === 0 &&
-                        <EmptyIndicator message='Sin usuarios para mostrar' />
-                    }
-
-                    {
-                        !loader && allUsers.length > 0 &&
-                        <RecordsList
-                            data={allUsers}
-                            activeDetail={activeDetail}
-                            onDetail={_id => setActiveDetail(_id)} />
-                    }
-                </div>
-
-                <div className="content-detail">
-                    <DetailRecords id={activeDetail} dateReport={dateReport} />
-                </div>
-            </div>
+              if (value) {
+                setDateReport(value)
+                window.sessionStorage.setItem('date_report', value)
+              }
+            }}
+          />
         </div>
-    )
+      </header>
+
+      <div className="content">
+        <div className="content-list">
+          {loader && <ActivityIndicator size={64} />}
+
+          {!loader && allUsers.length === 0 && (
+            <EmptyIndicator message="Sin usuarios para mostrar" />
+          )}
+
+          {!loader && allUsers.length > 0 && (
+            <RecordsList
+              data={allUsers}
+              activeDetail={activeDetail}
+              onDetail={_id => setActiveDetail(_id)}
+            />
+          )}
+        </div>
+
+        <div className="content-detail">
+          <DetailRecords id={activeDetail} dateReport={dateReport} />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Users
