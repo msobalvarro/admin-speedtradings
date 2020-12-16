bf23631c0af97f2734ff724615101c0bf03dc09a
Cesar Garay <cgaray.aguilar97@gmail.com>
2020-12-16 15:52:26 -0600

    Funcionalidad de buscar usuarios por correo y por pais  implementada

--- src/components/RecordsList/RecordsList.jsx
+++ src/components/RecordsList/RecordsList.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useMemo } from 'react'
 import ActivityIndicator from '../../components/ActivityIndicator/Activityindicator'
 import EmptyIndicator from '../../components/EmptyIndicator/EmptyIndicator'
 // Import constant
@@ -13,34 +13,19 @@ const RecordsList = ({
   activeDetail = -1,
   onDetail = _ => {},
 }) => {
-  const [filter, setFilter] = useState('')
+  const [keyword, setKeyword] = useState('')
 
-  // Componente que representa un articulo de la lista
-  // Registos
-  const itemRecord = (item, index) => {
-    if (
-      (item.name.length > 0 && item.name.toLowerCase().search(filter) > -1) ||
-      (item.country.length > 0 &&
-        item.country.toLowerCase().search(filter) > -1)
-    ) {
-      return (
-        <div
-          className={`row ${activeDetail === item.id_user ? 'active' : ''}`}
-          key={index}
-          onClick={_ => onDetail(item.id_user)}
-        >
-          <span className="icon-container">
-            {item.type_users && <UserIcon type={item.type_users} />}
-          </span>
-          <span className="name">{item.name}</span>
-          <span>{item.country}</span>
-          <span>
-            <Moment date={item.start_date} format="YYYY-MM" />
-          </span>
-        </div>
-      )
-    }
-  }
+  const filteredUsers = useMemo(
+    () =>
+      data.filter(user => {
+        return (
+          user.name.toLowerCase().includes(keyword.toLocaleLowerCase()) ||
+          user.user_email.toLowerCase().includes(keyword.toLocaleLowerCase()) ||
+          user.country.toLowerCase().includes(keyword.toLocaleLowerCase())
+        )
+      }),
+    [data, keyword]
+  )
 
   if (loader)
     return (
@@ -63,8 +48,8 @@ const RecordsList = ({
         <h2 className="title">Lista de usuarios</h2>
 
         <input
-          value={filter}
-          onChange={e => setFilter(e.target.value)}
+          value={keyword}
+          onChange={e => setKeyword(e.target.value)}
           placeholder="Escribe para buscar.."
           type="text"
           className="text-input"
@@ -86,13 +71,30 @@ const RecordsList = ({
         <div className="header">
           <span>Tipo</span>
           <span>Nombre</span>
+          <span>Estado</span>
           <span>Pais</span>
           <span>Fecha</span>
         </div>
 
-        {data
+        {filteredUsers
           .sort((a, b) => new Date(b.start_date) - new Date(a.start_date))
-          .map(itemRecord)}
+          .map((item, index) => (
+            <div
+              className={`row ${activeDetail === item.id_user ? 'active' : ''}`}
+              key={index}
+              onClick={_ => onDetail(item.id_user)}
+            >
+              <span className="icon-container">
+                {item.type_users && <UserIcon type={item.type_users} />}
+              </span>
+              <span className="name">{item.name}</span>
+              <span>{item.reviewed < 1 ? 'Sin verificar' : 'Verificado'}</span>
+              <span>{item.country}</span>
+              <span>
+                <Moment date={item.start_date} format="YYYY-MM" />
+              </span>
+            </div>
+          ))}
       </div>
     </>
   )
